dc0a7da9f7fef04beb7bbb4a45e70c7577d82821
mcdoash <mcdoash@users.noreply.github.com>
2025-11-07 21:19:23 -0500

    Basic neighbours & rule

--- automata.js
+++ automata.js
@@ -3,6 +3,7 @@ const Cell = class {
   col;
   id;
   state;
+  neighbours;
 
   constructor(r, c, initState) {
     this.row = r;
@@ -15,13 +16,21 @@ const Cell = class {
     this.state = newState;
     $('#' + this.id).attr('class', newState);
   }
+
+  /**
+   * Get the number of the cell's neighbours at a given state
+   * @param {string} state the state to check
+   * @returns {number} the number of neighbours at given state
+   */
+  numNeighAt(state) {
+    return this.neighbours.reduce((a, c) => a + (c.state == state ? 1 : 0), 0);
+  }
 };
 
 const CA = class {
   cells = [];
   states = [];
   transitions = {};
-  neighbours = {};
 
   constructor(states) {
     this.states = states;
@@ -33,6 +42,28 @@ const CA = class {
         this.cells[r][c] = new Cell(r, c, this.states[1]);
       }
     }
+    this.calcNeighbours();
+  }
+
+  /**
+   * Basic neighbour identification
+   * @todo more efficient method
+   */
+  calcNeighbours() {
+    for (let row = 0; row < rowNum; row++) {
+      for (let col = 0; col < colNum; col++) {
+        let neighbours = [this];
+
+        for (let i = row - 1; i <= row + 1; i++) {
+          for (let j = col - 1; j <= col + 1; j++) {
+            if ((i != row || j != col) && i >= 0 && i < rowNum && j >= 0 && j < colNum) {
+              neighbours.push(this.cells[i][j]);
+            }
+          }
+        }
+        this.cells[row][col].neighbours = neighbours;
+      }
+    }
   }
 
   /**
@@ -41,14 +72,15 @@ const CA = class {
   randomizeStates() {
     for (let r = 0; r < rowNum; r++) {
       for (let c = 0; c < colNum; c++) {
-        const newState = Math.floor(Math.random() * this.states.length);
+        let newState = Math.floor(Math.random() * (this.states.length + 1));
+        if (newState >= this.states.length) newState = 1;
         this.cells[r][c].setState(this.states[newState]);
       }
     }
   }
 };
 
-const rowNum = 10, colNum = 10;
+const rowNum = 30, colNum = 30;
 let M = new CA(['on', 'off']);
 setupElements();
 M.randomizeStates();
@@ -77,12 +109,36 @@ function setupElements() {
         else {
           M.cells[r][c].setState(M.states[0]);
         }
+        M.cells[r][c].numNeighAt('on');
       });
-
       $(grid).append(cell);
     }
   }
 
   // Set randomize button
-  $('button#randomize').on('click', () => M.randomizeStates());
+  $('button#randomize').on('click', () => {
+    M.randomizeStates();
+    // setTimeout(test, 5000);
+  });
+  $('button#iterate').on('click', () => {
+    iterate();
+  });
+}
+
+/**
+ *
+ */
+function iterate() {
+  for (let r = 0; r < rowNum; r++) {
+    for (let c = 0; c < colNum; c++) {
+      if (M.cells[r][c].numNeighAt('on') >= 4) {
+        // change = true;
+        M.cells[r][c].setState('on');
+      }
+      else if (M.cells[r][c].state == 'on') {
+        // change = true;
+        M.cells[r][c].setState('off');
+      }
+    }
+  } // only change after iteration
 }
